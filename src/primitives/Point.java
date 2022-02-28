28c3efd32bdd5f8d2bc5c5e31b517c747a2594a3
Eliana Grajower <grajower@g.jct.ac.il>
2022-02-28 19:33:53 +0200

    Initial commit

---
+++ src/primitives/Point.java
@@ -0,0 +1,34 @@
+package primitives;
+
+public class Point {
+
+    protected final Double3 xyz;
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x,y,z);
+    }
+
+    public Point(Double3 xyz){
+        this.xyz = xyz;
+    }
+
+    public Point add(Vector vector){
+        return new Point(xyz.add(vector.xyz));
+    }
+
+    public Vector subtract(Point point){
+        return new Vector(xyz.subtract(point.xyz));
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point)o;
+        return Object.equals(xyz, point.xyz);
+    }
+
+    @Override
+    public int hashCode() {
+        return Object.hash(xyz);
+    }
+}
---
+++ src/primitives/Vector.java
@@ -0,0 +1,27 @@
+package primitives;
+
+public class Vector extends Point {
+    public Vector(double x, double y, double z){
+        super(x, y, z);
+    }
+
+    public double lengthSquared(){
+    }
+
+    public double length(){
+
+    }
+
+    public double dotProduct(Vector vector){
+
+    }
+
+    public Vector crossProduct(Vector vector){
+
+    }
+
+    public Vector normalize(){
+
+    }
+
+}
