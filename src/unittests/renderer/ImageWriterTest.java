7f64ad3b5eaf403e62c139bb192175eb61fd2f1e
Eliana Grajower <grajower@g.jct.ac.il>
2022-04-26 21:34:48 +0300

    continuing...

--- src/unittests/renderer/ImageWriterTest.java
+++ src/unittests/renderer/ImageWriterTest.java
@@ -2,6 +2,8 @@ package unittests.renderer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import primitives.Color;
+import renderer.ImageWriter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +11,20 @@ class ImageWriterTest {
 
     @Test
     void testWriteToImage() {
-    }
-
-    @Test
-    void testWritePixel() {
+        Color yellow = new Color(255d,255d,0d);
+        Color red = new Color(255d,0d,0d);
+        ImageWriter imageWriter = new ImageWriter("yellowSubmarine",800,500);
+        //print red tiles on a yellow background
+        for (int i = 0; i < 800; i++) {
+            for (int j = 0; j < 500; j++) {
+                if(i % 50 == 0 || j % 50 == 0) {
+                    imageWriter.writePixel(i, j, red);
+                }
+                else {
+                    imageWriter.writePixel(i,j,yellow);
+                }
+            }
+        }
+        imageWriter.writeToImage();
     }
 }
